717ded645f23e1fb4e5aaea3125d453cb338f878
anunknowperson <kiselev.sereja@gmail.com>
2023-10-14 02:32:11 +0300

    Add CMake, vcpkg, Vulkan, glfw, glm.

---
+++ src/main.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+void create_vulkan() {
+    VkInstance instance;
+
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan Test";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
+    }
+
+    vkDestroyInstance(instance, nullptr);
+}
+
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Set GLFW to not create an OpenGL context
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Test", nullptr, nullptr);
+
+    if (!window) {
+        std::cerr << "Failed to create a GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    create_vulkan();
+
+    // Use GLM to perform a simple vector operation
+    glm::vec3 vector1(1.0f, 0.0f, 0.0f);
+    glm::vec3 vector2(0.0f, 1.0f, 0.0f);
+    glm::vec3 result = glm::cross(vector1, vector2);
+
+    std::cout << "Result of the cross product: (" << result.x << ", " << result.y << ", " << result.z << ")" << std::endl;
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
