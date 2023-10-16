756f37554e5db7d53d63d117fbeac9408ddb92ea
anunknowperson <kiselev.sereja@gmail.com>
2023-10-16 20:03:53 +0300

    Add validation layer, add basic extensions logic

--- include/internal/vulkan_instance.h
+++ include/internal/vulkan_instance.h
@@ -4,18 +4,50 @@
 #include "GLFW/glfw3.h"
 
 #include "vulkan/vulkan.h"
+#include <vector>
 
 class VulkanInstance {
 
 private:
 	VkInstance instance;
-		
+
+	VkDebugUtilsMessengerEXT debugMessenger;
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
+
+	const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+	};
+
+
+
 	VkApplicationInfo getAppInfo();
-	VkInstanceCreateInfo getInstanceCreateInfo(VkApplicationInfo& appInfo);
+	void populateInstanceCreateInfo(VkInstanceCreateInfo& createInfo, VkApplicationInfo& appInfo);
 
 	void enumerateExtensions();
 
+	void enumerateValidationLayers();
+	bool checkValidationLayerSupport();
+
+	std::vector<const char*> getRequiredExtensions();
+
 	void createInstance();
+	void setupDebugMessenger();
+
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData);
 
 public:
 	VulkanInstance();
