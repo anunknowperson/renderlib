60a43f6ed7ec6e5a0b87a604b968fd2ff7d04f88
Garaev Tagir <tgaraev653@gmail.com>
2023-12-15 22:11:27 +0300

    Basic parent system

--- src/scene/ParentSystem.cpp
+++ src/scene/ParentSystem.cpp
@@ -1,44 +1,148 @@
-#include "scene/ParentComponent.h"
-#include "core/logging.h"
+#include "scene/ParentSystem.h"
 
-struct ParentSystem {
-    flecs::query<Parent> ParentQuery;
-    flecs::query<Child> ChildQuery;
-    flecs::query<PreviousParent> PreviousParentQuery;
-
-    int FindChildIndex(const Child &children, const flecs::entity &entity)
-    {
-        for (size_t i = 0; i != children.Child.size(); i++) {
-            if (children.Child[i] == entity) {
-                return i;
-            }
+namespace {
+void updateParent(flecs::entity e, Parent& p)
+{
+    if (!p.parent.is_alive() || !p.parent.has<Child>()) {
+        e.destruct();
+    } else {
+        if (p.parent.has<Child>()) {
+            auto *child = p.parent.get_mut<Child>();
+            child->children.push_back(e);
         }
-        LOGE("Could not find child index");
-        return -1;
+    }
+}
+
+void updateChild(flecs::entity e, Child& c)
+{
+    auto& children = c.children;
+    auto newEnd = std::remove_if(
+            children.begin(),
+            children.end(),
+            [](const flecs::entity& child) { return (!child.is_alive() || !child.has<Parent>()); }
+    );
+    children.erase(newEnd, children.end());
+}
+
+void removeChild(flecs::entity e, Child& c)
+{
+    if (c.children.empty()) {
+        e.remove<Child>();
+    }
+}
+
+void changeParent(flecs::entity e, Parent& p, PreviousParent& pp)
+{
+    if (pp.parent.is_alive() and pp.parent.has<Child>()) {
+        auto *child = pp.parent.get_mut<Child>();
+        auto newEnd = std::remove_if(
+                child->children.begin(),
+                child->children.end(),
+                [e](const flecs::entity& child) { return child == e; }
+        );
+        child->children.erase(newEnd, child->children.end());
+    }
+    if (p.parent.is_alive()) {
+        auto *child = p.parent.get_mut<Child>();
+        child->children.push_back(e);
+    }
+    p.parent = pp.parent;
+    e.remove<PreviousParent>();
+}
+}
+
+void setRelation(flecs::entity child, flecs::entity parent)
+{
+    if (!parent.is_alive()) {
+#ifndef NDEBUG
+        LOGW("Trying to set entity %s as parent, but it is not alive", parent.name().c_str());
+#endif
+        return;
+    }
+    if (!child.is_alive()) {
+#ifndef NDEBUG
+        LOGWF("Trying to set entity {} as child, but it is not alive", child.name().c_str());
+#endif
+        return;
+    }
+    if (child == parent) {
+#ifndef NDEBUG
+        LOGWF("Trying to set entity {} as its own parent", child.name().c_str());
+#endif
+        return;
     }
 
-    void RemoveChildFromParent(flecs::world& world, flecs::entity parentEntity, flecs::entity childEntity) {
-        if (!parentEntity.has<Child>()) {
-            LOGW("Trying to remove a child from a parent that does not have a Child component");
+    if (child.has<Parent>())
+    {
+        if (child.get<Parent>()->parent == parent) {
+            return;
+        } else {
+            child.set<PreviousParent>({child.get<Parent>()->parent});
+        }
+    }
+    if (parent.has<Child>())
+    {
+        auto &children = parent.get_mut<Child>()->children;
+        if (std::find(children.begin(),
+                      children.end(),
+                      child) != children.end()) {
             return;
         }
-        auto children = parentEntity.get_mut<Child>();
-        int childIndex = -1;
-        for (size_t i = 0; i != children->Child.size(); ++i) {
-            if (children->Child[i] == childEntity) {
-                childIndex = i;
-                break;
-            }
-        }
-        if (childIndex != -1) {
-            children->Child.erase(children->Child.begin() + childIndex);
-        }
-        if (children->Child.size() == 0) {
-            parentEntity.remove<Child>();
-        }
+        children.push_back(child);
     }
-};
+    child.set<Parent>({parent});
+}
 
-struct GatherChangedParents {
+// make docs for overloads
+void removeRelation(flecs::entity child)
+{
+    if (!child.is_alive() || !child.has<Parent>()) {
+#ifndef NDEBUG
+        LOGWF("Trying to remove parent from entity {}, but it is not alive or has no parent", child.name().c_str());
+#endif
+        return;
+    }
+    child.remove<Parent>();
+}
 
-};
+// make docs for overloads
+void removeRelation(flecs::entity removing_child, flecs::entity parent) {
+    if (!parent.is_alive() || !parent.has<Child>() || !parent.get<Child>()->children.empty()) {
+#ifndef NDEBUG
+        LOGWF("Trying to remove entity {} from parent {}, but parent is not alive or has no children",
+              removing_child.name().c_str(), parent.name().c_str());
+#endif
+    }
+    if (!removing_child.is_alive() || !removing_child.has<Parent>()) {
+#ifndef NDEBUG
+        LOGWF("Trying to remove entity {} from parent {}, but child is not alive or has no parent",
+              removing_child.name().c_str(), parent.name().c_str());
+#endif
+    }
+    auto &children = parent.get_mut<Child>()->children;
+    auto newEnd = std::remove_if(
+            children.begin(),
+            children.end(),
+            [removing_child](const flecs::entity& child) { return child == removing_child; }
+    );
+}
+
+// fix definition
+void HierarchySystem(flecs::world &world)
+{
+    world.system<Parent>()
+            .kind(flecs::OnAdd)
+            .each(updateParent);
+
+    world.system<Child>()
+            .kind(flecs::OnSet)
+            .each(updateChild);
+
+    world.system<Child>()
+            .kind(flecs::OnRemove)
+            .each(removeChild);
+
+    world.system<Parent, PreviousParent>()
+            .kind(flecs::OnAdd)
+            .each(changeParent);
+}
