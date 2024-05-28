4860b73867a4b5a0c89cc670f2d90587608c6c7b
anunknowperson <kiselev.sereja@gmail.com>
2024-05-28 18:34:52 +0300

    dsadasaddas

---
+++ src/core/Mesh.cpp
@@ -0,0 +1,13 @@
+//
+// Created by User on 28.05.2024.
+//
+
+#include "Mesh.h"
+
+#include "graphics/vulkan/vk_engine.cpp"
+
+Mesh::Mesh(std::filesystem::path filePath) {
+
+
+
+}
---
+++ src/core/Mesh.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+#include <filesystem>
+
+class Mesh {
+
+    Mesh(std::filesystem::path filePath);
+
+};
