c6e4096bb243906f5a58a1098ede120b67a326fb
Tydik <tgaraev653@gmail.com>
2024-05-29 14:46:20 +0300

    update: code formatting

--- src/graphics/vulkan/vk_images.h
+++ src/graphics/vulkan/vk_images.h
@@ -1,13 +1,13 @@
 
-#pragma once 
+#pragma once
 
 #include <vulkan/vulkan.h>
 
 namespace vkutil {
 
-	void transition_image(VkCommandBuffer cmd, VkImage image, VkImageLayout currentLayout, VkImageLayout newLayout);
+void transition_image(VkCommandBuffer cmd, VkImage image, VkImageLayout currentLayout, VkImageLayout newLayout);
 
-    void
-    copy_image_to_image(VkCommandBuffer cmd, VkImage source, VkImage destination, VkExtent2D srcSize,
-                        VkExtent2D dstSize);
+void
+copy_image_to_image(VkCommandBuffer cmd, VkImage source, VkImage destination, VkExtent2D srcSize,
+		VkExtent2D dstSize);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,19 @@
 #include "core/Model.h"
 #include "core/View.h"
 
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* args[])
+{
+	try
+	{
+		auto const model = createModel();
+		auto const controller = createController(model);
+		auto const view = createView(controller, model);
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *args[]) {
+		view->run();
+	} catch (std::runtime_error const& e)
+	{
+		std::cerr << "Unhandled exception: " << e.what() << '\n';
+	}
 
-    try {
-        auto const model = createModel();
-        auto const controller = createController(model);
-        auto const view = createView(controller, model);
-
-
-        view->run();
-    } catch (std::runtime_error const &e) {
-        std::cerr << "Unhandled exception: " << e.what() << '\n';
-    }
-
-    return 0;
+	return 0;
 }
