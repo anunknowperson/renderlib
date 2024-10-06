59ede4d78ca578d78310d36111292b9d014ff3f3
Pand <officialpand@gmail.com>
2024-10-06 20:39:21 +0300

    CameraController

---
+++ src/core/CameraController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by offic on 10/6/2024.
+//
+
+#include "CameraController.h"
---
+++ src/core/createInstance.cpp
@@ -0,0 +1,11 @@
+#include "interfaces/IController.h"
+#include "interfaces/IModel.h"
+#include "core/Controller.h"
+#include "core/Model.h"
+
+IController::Ptr createInstance()
+{
+	const auto model = createModel();
+	const auto controller = createController(model);
+	return controller;
+}
