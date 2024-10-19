783543658d89ba5f4a1e70b19ab4d0b78228a5ca
Pand <officialpand@gmail.com>
2024-10-19 12:34:41 +0300

    CameraController .cpp & .h

---
+++ src/include/core/CameraController.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <SDL.h>
+#include "scene/Camera.h"
+
+class CameraController {
+public:
+    explicit CameraController(Camera& camera);
+
+    void setPosition(const glm::vec3& newPosition);
+    glm::vec3 getPosition() const;
+
+    void setRotation(const glm::quat& newRotation);
+    glm::quat getRotation() const;
+    
+    void setPitch(float newPitch);
+    void setYaw(float newYaw);
+    float getPitch() const;
+    float getYaw() const;
+
+    void lookAt(const glm::vec3& target);
+    void setFOV(float newFOV);
+    float getFOV() const;
+
+    glm::mat4 getViewMatrix() const; // Метод для получения матрицы вида
+    void updateViewMatrix(); // Метод для обновления матрицы вида
+
+    void processSDLEvent(const SDL_Event& event); // Метод для обработки событий SDL
+
+private:
+    Camera& camera; // Ссылка на камеру
+    float pitch; // Угол наклона
+    float yaw; // Угол поворота
+};
