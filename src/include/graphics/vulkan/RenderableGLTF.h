0ec9011bf9fc3647f656c95f7e78d6b79e051b4c
Artyom Gavrilov <rzdartem14@gmail.com>
2024-11-29 10:01:41 +0300

    chore: remove useless commented lines of code

--- src/include/graphics/vulkan/RenderableGLTF.h
+++ src/include/graphics/vulkan/RenderableGLTF.h
@@ -7,49 +7,8 @@
 #include "vk_types.h"
 #include "scene/Mesh.h"
 
-// struct GLTFMaterial {
-//     MaterialInstance data;
-// };
-//
-// struct GeoSurface {
-//     uint32_t startIndex;
-//     uint32_t count;
-//     std::shared_ptr<GLTFMaterial> material;
-// };
-//
-// struct MeshAsset {
-//     std::string name;
-//
-//     std::vector<GeoSurface> surfaces;
-//     GPUMeshBuffers meshBuffers;
-// };
-
-// forward declaration
-// class VulkanEngine;
-
-// в vk_engine.cpp
-// std::optional<std::vector<std::shared_ptr<MeshAsset>>> loadGltfMeshes(
-//         VulkanEngine* engine, std::filesystem::path filePath);
-
 class RenderableGLTF : public IRenderable {
 public:
-    // // storage for all the data on a given glTF file
-    // std::unordered_map<std::string, std::shared_ptr<MeshAsset>> meshes;
-    // std::unordered_map<std::string, std::shared_ptr<ENode>> nodes;
-    // std::unordered_map<std::string, AllocatedImage> images;
-    // std::unordered_map<std::string, std::shared_ptr<GLTFMaterial>> materials;
-    //
-    // // nodes that dont have a parent, for iterating through the file in tree
-    // // order
-    // std::vector<std::shared_ptr<ENode>> topNodes;
-    //
-    // std::vector<VkSampler> samplers;
-    //
-    // DescriptorAllocatorGrowable descriptorPool;
-    //
-    // AllocatedBuffer materialDataBuffer;
-    //
-    // VulkanEngine* creator;
     using LoadedGltfPtr = std::shared_ptr<const Mesh::GLTF::LoadedGLTF>;
     explicit RenderableGLTF(LoadedGltfPtr gltf);
 
