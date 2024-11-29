db5d170a7a0a2900bfbba7451ed275ba1fb399a1
Artyom Gavrilov <rzdartem14@gmail.com>
2024-11-29 10:25:07 +0300

    chore: delete useless commented lines of code

--- src/include/interfaces/IModel.h
+++ src/include/interfaces/IModel.h
@@ -83,27 +83,5 @@ public:
 
      virtual VulkanEngine& get_engine() = 0;
 
-    /*! \brief
-     * Gets the chip handler from HIDAPI required to change the settings by the
-     * Controller
-     * @return
-     * Returns the device handler in case, can't be nullptr
-     */
-    //[[nodiscard]] virtual hid_device* getChipHandler() const = 0;
-
-    /*! \brief
-     * Requests from chip the level of brightness
-     * @return
-     * Returns the level of brightness [0; 100]
-     */
-    // virtual uint8_t getBrightness() = 0;
-
-    /*! \brief
-     * Requests RGB values from the chip
-     * @return
-     * Returns the struct with .R, .G, .B fields
-     */
-    // virtual Color getRGB() = 0;
-
     using Ptr = std::shared_ptr<IModel>;
 };
