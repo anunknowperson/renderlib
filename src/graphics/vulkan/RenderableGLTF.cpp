15f375d786da25fc78e24b38e73e391e46abec42
Artyom Gavrilov <rzdartem14@gmail.com>
2024-11-29 08:50:38 +0300

    refactor: remove an useless function
    Function that creates RenderableGLTF can be
    replaced with the constructor of the object.

--- src/graphics/vulkan/RenderableGLTF.cpp
+++
@@ -1,7 +0,0 @@
-#include "graphics/vulkan/RenderableGLTF.h"
-#include "graphics/vulkan/vk_loader.h"
-
-IRenderable::Ptr createRenderableGLTF(RenderableGLTF::LoadedGltfPtr ptr)
-{
-    return std::make_shared<RenderableGLTF>(std::move(ptr));
-}
