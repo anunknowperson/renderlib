303f80d24c1c84862337afdfc56cb4d364827cec
Artyom Gavrilov <rzdartem14@gmail.com>
2024-11-30 08:37:51 +0300

    chore: add description to IModel

--- src/include/interfaces/IModel.h
+++ src/include/interfaces/IModel.h
@@ -40,29 +40,29 @@ public:
     virtual void registerWindow(struct SDL_Window* window) = 0;
 
     /*!
-     * \brief Creates a new mesh with the given name.
-     *
-     * \param engine
-     * \param file_path
-     *
-     * This method creates a new mesh identified by the provided name.
+     * \brief Loads (parses) a new mesh to the model
+     * \param file_path The path to the mesh
+     * \return Returns render-id of the mesh that helps identify the provided mesh.
      */
     virtual Mesh::rid_t createMesh(const std::filesystem::path& file_path) = 0;
-
+    /*!
+     * \brief Deletes the provided mesh from a storage
+     * @param rid Render-id of the mesh to be deleted
+     */
     virtual void delete_mesh(Mesh::rid_t rid) = 0;
 
     /*!
-     * \brief Sets the transformation matrix for a mesh.
-     *
-     * \param rid
+     * \brief Sets the transformation matrix for a mesh identified by render-id
+     * \param rid Render-id of the mesh to be transformed
      * \param transform Transformation matrix to be applied to the mesh.
-     *
-     * This method sets the transformation matrix for the mesh identified by the
-     * provided name.
      */
     virtual void setMeshTransform(Mesh::rid_t rid, glm::mat4x4 transform) = 0;
-
-    virtual glm::mat4 get_mesh_transform(Mesh::rid_t) = 0;
+     /*!
+    * \brief Gets the transformation matrix of a mesh.
+    * \param rid Render-id of the mesh
+    * \return Returns a transform matrix of the mesh
+    */
+    virtual glm::mat4 get_mesh_transform(Mesh::rid_t rid) = 0;
 
     struct MeshPair {
      std::shared_ptr<const Mesh::GLTF::LoadedGLTF> ptr;
