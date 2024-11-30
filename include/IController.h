b585f2ca83da54976e7a4324cc251bc74775f8cc
Artyom Gavrilov <rzdartem14@gmail.com>
2024-11-30 07:34:11 +0300

    chore: add description to ICtlr

--- include/IController.h
+++ include/IController.h
@@ -24,12 +24,23 @@ public:
      * \brief Virtual destructor for the interface.
      */
     virtual ~IController() = default;
-
+    /*!
+     * \brief Renders a new frame
+     */
     virtual void run() = 0;
+    /*!
+     * \brief Applies updates
+     */
     virtual void update() = 0;
-
+    /*!
+     * \brief Gets a controller to interact with meshes
+     * \return Pointer to the MeshController
+     */
     [[nodiscard]] virtual std::weak_ptr<const MeshController> getMeshController() = 0;
-
+    /*!
+     * \brief Handles user actions (SDL events)
+     * @param e SDL event
+     */
     virtual void process_event(const SDL_Event& e) = 0;
     /*!
      * \brief Shared pointer type for IController.
