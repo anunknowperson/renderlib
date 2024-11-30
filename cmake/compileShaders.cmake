231544618fb77d50c8b29687cf3f645877d6763a
Artyom Gavrilov <rzdartem14@gmail.com>
2024-11-30 09:45:57 +0300

    revert old commit to save cmake

--- cmake/compileShaders.cmake
+++ cmake/compileShaders.cmake
@@ -1,7 +1,7 @@
 set(BUILD_SHADERS_SCRIPT "${CMAKE_SOURCE_DIR}/scripts/build_shaders.py")
 
 set(SHADERS_SOURCE_DIR "${CMAKE_SOURCE_DIR}/shaders")
-set(SHADERS_DEST_DIR "${CMAKE_BINARY_DIR}/demo/shaders")
+set(SHADERS_DEST_DIR "${CMAKE_BINARY_DIR}/shaders")
 
 add_custom_command(TARGET ${PROJECT_NAME} PRE_BUILD
         COMMAND ${CMAKE_COMMAND} -E echo "Running build_shaders.py script..."
