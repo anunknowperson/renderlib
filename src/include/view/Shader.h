87b7840c207ae70dc318ee17a0a23cdeea286b64
Korbelainen Daniel <st117350@student.spbu.ru>
2024-12-07 12:10:11 +0300

    requested changes

--- src/include/view/Shader.h
+++ src/include/view/Shader.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <string>
+#include <filesystem>
 #include <vulkan/vulkan.h>
 
 // Класс Shader для загрузки, хранения и управления шейдерами в Vulkan
 class Shader {
 public:
     // Конструктор, принимает путь к файлу шейдера и устройство Vulkan
-    Shader(const std::string& shaderPath, VkDevice device);
+    Shader(const std::filesystem::path& shaderPath, VkDevice device);
 
     // Метод для получения Vulkan-модуля шейдера
     [[nodiscard]] VkShaderModule getShaderModule() const;
@@ -17,7 +17,7 @@ public:
 
 private:
     // Метод для загрузки данных шейдера и создания модуля шейдера
-    bool loadShaderModule(const std::string& filePath,
+    bool loadShaderModule(const std::filesystem::path& filePath,
                           VkShaderModule* outShaderModule);
 
     VkDevice _device;  // Устройство Vulkan, необходимое для создания шейдеров
--- src/view/Shader.cpp
+++ src/view/Shader.cpp
@@ -5,33 +5,34 @@
 #include <vector>
 
 // Конструктор, принимает путь к файлу шейдера и устройство Vulkan
-Shader::Shader(const std::string& shaderPath, VkDevice device)
+Shader::Shader(const std::filesystem::path& shaderPath, VkDevice device)
     : _device(device), _shaderModule(VK_NULL_HANDLE) {
     if (!loadShaderModule(shaderPath, &_shaderModule)) {
-        fmt::print("Ошибка при создании шейдера из {}\n", shaderPath);
+        fmt::print("Ошибка при создании шейдера из {}\n", shaderPath.string());
     }
 }
 
 // Загрузка данных шейдера и создание VkShaderModule
-bool Shader::loadShaderModule(const std::string& filePath,
+bool Shader::loadShaderModule(const std::filesystem::path& filePath,
                               VkShaderModule* outShaderModule) {
     std::ifstream file(filePath, std::ios::ate | std::ios::binary);
     if (!file.is_open()) {
-        fmt::print("Не удалось открыть файл шейдера {}\n", filePath);
+        fmt::print("Не удалось открыть файл шейдера {}\n", filePath.string());
         return false;
     }
 
-    size_t fileSize = static_cast<size_t>(file.tellg());
-    std::vector<char> buffer(fileSize);
+    const auto fileSize = file.tellg();
+    std::vector<char> buffer(static_cast<size_t>(fileSize));
 
     file.seekg(0);
-    file.read(buffer.data(), static_cast<std::streamsize>(fileSize));
+    file.read(buffer.data(), fileSize);
     file.close();
 
-    VkShaderModuleCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-    createInfo.codeSize = buffer.size();
-    createInfo.pCode = reinterpret_cast<const uint32_t*>(buffer.data());
+    VkShaderModuleCreateInfo createInfo{
+            .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+            .codeSize = buffer.size(),
+            .pCode = reinterpret_cast<const uint32_t*>(buffer.data())
+    };
 
     if (vkCreateShaderModule(_device, &createInfo, nullptr, outShaderModule) !=
         VK_SUCCESS) {
