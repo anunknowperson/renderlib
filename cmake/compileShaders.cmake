f368a59de486df37437d17febab2f8db6081a6bb
JonnyDevp <walacues@gmail.com>
2024-12-26 02:28:23 +0300

    Исправлены замечания по пулл-реквесту: улучшена обработка ошибок, обновлены комментарии

--- cmake/compileShaders.cmake
+++ cmake/compileShaders.cmake
@@ -1,6 +1,10 @@
 set(BUILD_SHADERS_SCRIPT "${CMAKE_SOURCE_DIR}/scripts/build_shaders.py")
+set(SHADERS_SOURCE_DIR "${CMAKE_SOURCE_DIR}/shaders")
+set(SHADERS_DEST_DIR "${CMAKE_BINARY_DIR}/shaders")
 
 add_custom_command(TARGET ${PROJECT_NAME} PRE_BUILD
-    COMMAND ${CMAKE_COMMAND} -E echo "Running build_shaders.py script..."
-    COMMAND ${CMAKE_COMMAND} -E env PROJECTDIR=${CMAKE_SOURCE_DIR} python ${BUILD_SHADERS_SCRIPT}
-    COMMENT "Running build_shaders.py")
+        COMMAND ${CMAKE_COMMAND} -E echo "Running build_shaders.py script..."
+        COMMAND python ${BUILD_SHADERS_SCRIPT} 
+                ${SHADERS_SOURCE_DIR}
+                ${SHADERS_DEST_DIR}
+        COMMENT "Running build_shaders.py")
