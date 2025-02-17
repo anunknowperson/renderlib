208ec1e6f77d636757883e38c10e2c10f4572591
Nikita <6u8654u4@gmail.com>
2025-02-17 20:05:22 +0300

    deleted unnecessary [[nodiscard]]

--- src/include/graphics/vulkan/vk_swapchain.h
+++ src/include/graphics/vulkan/vk_swapchain.h
@@ -1,52 +1,52 @@
 #pragma once
 
+#include "SDL2/SDL_vulkan.h"
 #include "interfaces/ISwapchain.h"
 #include "vk_types.h"
-#include "SDL2/SDL_vulkan.h"
 
 class SwapchainController : public ISwapchainController {
 public:
     SwapchainController(std::shared_ptr<VulkanContext> ctx,
-                        VmaAllocator allocator,
-                        SDL_Window* window);
+                        VmaAllocator allocator, SDL_Window* window);
 
     void create_swapchain(uint32_t width, uint32_t height) override;
     void destroy_swapchain() override;
     void resize_swapchain() override;
 
-    [[nodiscard]] const VkFormat* get_swapchain_image_format() const  override {
+    const VkFormat* get_swapchain_image_format() const override {
         return &_swapchainImageFormat;
     }
 
-    [[nodiscard]] VkExtent2D get_swapchain_extent() const override {
+    VkExtent2D get_swapchain_extent() const override {
         return _swapchainExtent;
     }
 
-    [[nodiscard]] VkSwapchainKHR get_swapchain() const override {
+    VkSwapchainKHR get_swapchain() const override {
         return _swapchain;
     }
 
-    [[nodiscard]] const VkSwapchainKHR* get_swapchain_ptr() const override {
+    const VkSwapchainKHR* get_swapchain_ptr() const override {
         return &_swapchain;
     }
 
-    [[nodiscard]] std::vector<VkImage> get_swapchain_images() const override {
+    std::vector<VkImage> get_swapchain_images() const override {
         return _swapchainImages;
     }
 
-    [[nodiscard]] VkImage get_swapchain_image_by_index(
+    VkImage get_swapchain_image_by_index(
             const uint32_t i) const override {
         return _swapchainImages[i];
     }
 
-    [[nodiscard]] VkImageView get_swapchain_image_view_by_index(
+    VkImageView get_swapchain_image_view_by_index(
             const uint32_t i) const override {
         return _swapchainImageViews[i];
     }
 
-    std::vector<VkImageView> get_swapchain_image_views() const  override {
+    std::vector<VkImageView> get_swapchain_image_views() const override {
         return _swapchainImageViews;
     }
+
 private:
     std::shared_ptr<VulkanContext> vCtx;
 
@@ -63,6 +63,8 @@ private:
 
 namespace vk_swapchain {
 
-SwapchainController::Ptr make_swapchain_controller(std::shared_ptr<VulkanContext> vCtx, VmaAllocator allocator, SDL_Window* window);
+SwapchainController::Ptr make_swapchain_controller(
+        std::shared_ptr<VulkanContext> vCtx, VmaAllocator allocator,
+        SDL_Window* window);
 
-} // namespace vk_swapchain
+}  // namespace vk_swapchain
