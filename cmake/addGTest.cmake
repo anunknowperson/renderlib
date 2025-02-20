bfa998b301776a948b5bdee070e2fecdb4a36b8b
Artyom Gavrilov <rzdartem13@gmail.com>
2025-02-21 02:49:45 +0300

    feat(test): download gtest with vcpkg

--- cmake/addGTest.cmake
+++ cmake/addGTest.cmake
@@ -1,13 +1,6 @@
-include(FetchContent)
 include(GoogleTest)
+find_package(GTest CONFIG REQUIRED)
 
-FetchContent_Declare(
-        googletest
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG        v1.15.2
-)
-
-FetchContent_MakeAvailable(googletest)
 add_library(GTest::GTest INTERFACE IMPORTED)
 target_link_libraries(GTest::GTest INTERFACE gtest_main)
 
