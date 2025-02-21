abf00dbfb5394ea35505627256c5661a0212f5d4
Artyom Gavrilov <rzdartem13@gmail.com>
2025-02-21 03:02:10 +0300

    fix: add gtest by vcpkg guide

--- cmake/addGTest.cmake
+++ cmake/addGTest.cmake
@@ -1,9 +1,6 @@
 include(GoogleTest)
 find_package(GTest CONFIG REQUIRED)
 
-add_library(GTest::GTest INTERFACE IMPORTED)
-target_link_libraries(GTest::GTest INTERFACE gtest_main)
-
 # keeps CACHE cleaner
 mark_as_advanced(
         BUILD_GMOCK BUILD_GTEST BUILD_SHARED_LIBS
@@ -16,7 +13,7 @@ macro(add_gtest TESTNAME)
     add_executable(${TESTNAME} ${ARGN})
     # link the Google test infrastructure, mocking library, and a default main function to
     # the test executable.  Remove gtest_main if writing your own main function.
-    target_link_libraries(${TESTNAME} ${PROJECT_NAME} gtest gmock gtest_main)
+    target_link_libraries(${TESTNAME} ${PROJECT_NAME} GTest::gtest GTest::gtest_main GTest::gmock GTest::gmock_main)
     # gtest_discover_tests replaces gtest_add_tests,
     # see https://cmake.org/cmake/help/v3.10/module/GoogleTest.html for more options to pass to it
     gtest_discover_tests(${TESTNAME}
