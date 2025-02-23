8c7260b02d26a042bd75cd64fd982676e3b45bf3
Tydik42 <tgaraev653@gmail.com>
2025-02-23 12:37:34 +0000

    chore(ci): checking the work

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,6 +8,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* args[]) {
     try {
         const auto controller = createInstance();
         controller->init();
+       int ABOBA = 42;
     } catch (const std::runtime_error& e) {
         std::cerr << "Unhandled exception: " << e.what() << '\n';
     }
