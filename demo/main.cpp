254888fa740b91b62904ac6d427b937def396856
Tydik42 <tgaraev653@gmail.com>
2025-02-23 12:54:12 +0000

    fix: remove checking lines in main.cpp, changed cpp version to 23

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,7 +8,6 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* args[]) {
     try {
         const auto controller = createInstance();
         controller->init();
-        [[maybe_unused]] int aboba = 42;
     } catch (const std::runtime_error& e) {
         std::cerr << "Unhandled exception: " << e.what() << '\n';
     }
