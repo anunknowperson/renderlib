760af669eeaa582a100ccf1e41f169765935acfe
Sergei Kiselev <54563399+anunknowperson@users.noreply.github.com>
2025-06-02 23:09:24 +0700

    Various fixes for meshes loading logic, model reload, new default models (#99)
    * Various fixes for meshes loading logic, model reload, new default models
    
    Fix gltf loading with 0 materials, Add more meshes, Fix default mesh,
    Add ability to load another model on mesh
    
    * Update vk_loader.cpp - Fix bad merge
    
    * Fix broken merge
    
    ---------
    
    Co-authored-by: anunknowperson <torvalds@linux-foundation.org>

--- src/include/core/Mesh.h
+++ src/include/core/Mesh.h
@@ -1,3 +1,4 @@
+// Mesh.h
 #pragma once
 
 #include <filesystem>
@@ -10,11 +11,15 @@ public:
     Mesh(const std::string& filePath);
     ~Mesh();
 
+    // Add model change functionality
+    void set_model(const std::string& filePath);
+    void remove_model();
+
     void set_transform(glm::mat4 t);
     glm::mat4 get_transform();
 
 private:
     glm::mat4 _transform;
-
+    std::string _currentModelPath;  // Track current model path
     int64_t _rid;
 };
