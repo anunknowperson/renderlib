87d7ab701dc914048316f123b96b347c4eabed5c
Grigoriy Klopov <139513740+Gr-i-niy@users.noreply.github.com>
2025-06-02 15:01:32 +0300

    feat: replace deletion_queue with smart pointers (#98)

--- src/graphics/vulkan/pipelines.cpp
+++ src/graphics/vulkan/pipelines.cpp
@@ -78,8 +78,8 @@ void GLTFMetallic_Roughness::build_opaque_pipeline(VulkanEngine* engine,
     pipelineBuilder.set_multisampling_none();
     pipelineBuilder.disable_blending();
     pipelineBuilder.enable_depthtest(true, VK_COMPARE_OP_GREATER_OR_EQUAL);
-    pipelineBuilder.set_color_attachment_format(engine->_drawImage.imageFormat);
-    pipelineBuilder.set_depth_format(engine->_depthImage.imageFormat);
+    pipelineBuilder.set_color_attachment_format(engine->_drawImage->get().imageFormat);
+    pipelineBuilder.set_depth_format(engine->_depthImage->get().imageFormat);
     pipelineBuilder._pipelineLayout = layout;
 
     opaquePipeline.pipeline = pipelineBuilder.build_pipeline(engine->_device);
