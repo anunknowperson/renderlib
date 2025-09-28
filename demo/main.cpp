042653447cfeeabc55cd8a32b94afc84eb1f43eb
Varnill <st117347@student.spbu.ru>
2025-09-28 23:46:48 +0300

    chore: delete useless createInstance

--- demo/main.cpp
+++ demo/main.cpp
@@ -3,10 +3,11 @@
 #include <stdexcept>
 
 #include "IController.h"
+#include "core/Controller.h"
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* args[]) {
     try {
-        const auto controller = createInstance();
+        const auto controller = createController();
         controller->run();
     } catch (const std::runtime_error& e) {
         std::cerr << "Unhandled exception: " << e.what() << '\n';
--- src/core/createInstance.cpp
+++
@@ -1,9 +0,0 @@
-#include "IController.h"
-#include "core/Controller.h"
-#include "core/Model.h"
-
-IController::Ptr createInstance() {
-
-    const auto controller = createController();
-    return controller;
-}
