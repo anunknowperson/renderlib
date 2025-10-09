a92e7b5bae0685f17ced55ac6ad64934631ba6de
Varnill <st117347@student.spbu.ru>
2025-10-09 21:42:39 +0300

    refactor: move LoaderGLTF to ModelImpl

--- src/include/scene/LoaderGLTF.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "scene/Mesh.h"
-
-struct LoaderGLTF {
-    static std::optional<const std::shared_ptr<const Mesh::GLTF::LoadedGLTF>> loadGLTF(
-            VulkanEngine& engine, std::string_view filePath);
-};
